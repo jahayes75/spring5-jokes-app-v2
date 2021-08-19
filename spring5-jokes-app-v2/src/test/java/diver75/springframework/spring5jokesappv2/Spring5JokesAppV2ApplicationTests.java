a0ae1225f25ee0b82a04c2c8cf6a2544e5d86adb
Jason Hayes <jahayes75@gmail.com>
2021-08-19 09:39:24 -0500

    Initial Commit

---
+++ spring5-jokes-app-v2/src/test/java/diver75/springframework/spring5jokesappv2/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package diver75.springframework.spring5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
